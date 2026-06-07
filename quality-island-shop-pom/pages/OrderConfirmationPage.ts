c428a8c90015a7016bd7fb7312a013d625989782
Witalij <vitaliy.nc1@gmail.com>
2026-06-07 14:02:54 +0200

    Reduce ran tests

--- quality-island-shop-pom/pages/OrderConfirmationPage.ts
+++ quality-island-shop-pom/pages/OrderConfirmationPage.ts
@@ -6,6 +6,6 @@ export class OrderConfirmationPage {
   constructor(private page: Page) {}
 
   async expectOrderConfirmationPageIsVisible() {
-    await expect(this.page.getByText('Thank you for your order!')).toBeVisible();
+    await expect(this.page.getByText('Order Confirmed!')).toBeVisible();
   }
 }
