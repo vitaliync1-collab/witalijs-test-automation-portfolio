3de9dfd897625d961b4ea9468be1a934c97c72da
Witalij <vitaliy.nc1@gmail.com>
2026-06-25 13:22:29 +0200

    failed login test

---
+++ quality-island-shop-pom/pages/LoginPage.ts
@@ -0,0 +1,28 @@
+import {expect, Locator, Page} from "@playwright/test";
+
+export class LoginPage {
+    constructor(private page: Page) {}
+    
+    async openLoginPage() {
+        await this.page.goto('https://quality-arena-labs.base44.app/shop/login');
+    }
+
+    async fillLoginForm(email: string, password: string) {
+        await this.page.getByLabel('Email').fill(email);
+        await this.page.getByTestId('login-password').fill(password);
+    }
+
+    async submitLoginForm() {
+        await this.page.getByRole('button', { name: 'Sign In' }).click();
+    }
+
+    async expectLoginPageIsVisible() {
+        await expect(this.page.getByText('Sign In to Shop')).toBeVisible();
+    }
+
+    async expectLoginFaledMessageIsVisible() {
+        await expect(this.page.getByText('Invalid email or password')).toBeVisible();
+    }
+
+    
+}
---
+++ quality-island-shop-pom/tests/failedLogintest.spec.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test('Verify login process with invalid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.openLoginPage();
+    await loginPage.fillLoginForm('invalid@example.com', 'invalidpassword');
+    await loginPage.submitLoginForm();
+    await loginPage.expectLoginFaledMessageIsVisible();
+    await loginPage.expectLoginPageIsVisible();
+});
